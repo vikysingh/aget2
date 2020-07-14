e9e3287f8160c334ecde2341a8dd80631c098081
Vikram Singh <51959049+vikysingh@users.noreply.github.com>
2020-07-14 11:49:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,51 +2,63 @@
 //
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector(".main_agency").addEventListener("click", () => {
-        agencyAnimate(".agency_section")
+    document.querySelector(".main_agency").addEventListener("click", e => {
+        agencyAnimate(".agency_section", e.target.className)
         // animateColorer()
     })
 
-    document.querySelector(".main_work").addEventListener("click", () => {
-        agencyAnimate(".main_project_section")
+    document.querySelector(".main_work").addEventListener("click", e => {
+        agencyAnimate(".main_project_section", e.target.className)
         // animateColorer()
     })
 
-    document.querySelector(".main_about").addEventListener("click", () => {
-        agencyAnimate(".about_section")
+    document.querySelector(".main_about").addEventListener("click", e => {
+        agencyAnimate(".about_section", e.target.className)
         // animateColorer()
     })
 })
 
 const txt = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus neque in assumenda, odio obcaecati quis reprehenderit."
+const bgColors = {
+    main_agency: "#2f434c",
+    main_work: "#ec1358",
+    main_about: "#5ebfea"
+}
 
-function agencyAnimate(element) {
+function agencyAnimate(element, colorClass) {
     let tl = gsap.timeline()
+    let transitioner = document.querySelector(".agency_transitioner")
+
+    transitioner.style.backgroundColor = bgColors[colorClass]
+
     tl
-        .to(".agency_transitioner", {
+        .to(transitioner, {
             duration: 0,
             display: "block"
         })
-        .to(".agency_transitioner", {
+        .to(transitioner, {
             // delay: 3,
-            duration: 0.9,
+            duration: 0.5,
             right: "0%"
         })
-        // .to(".agency_transitioner", {
-        //     duration: 0.9,
-        //     right: "0%"
-        // })
         .to(element, {
             // transform: 'scale(1)',
             duration: 0,
             display: "block",
-            width: "100vw"
+            width: "100vw",
+            zIndex: 2
             // delay: 5
         })
-        .to(".agency_transitioner", {
-            backgroundColor: "#1890ff",
-            duration: 0.9,
-            left: "-110%"
+        .to(".main_agency h1, .main_work, .main_about", {
+            zIndex: 0
+        })
+        .to(transitioner, {
+            // backgroundColor: "#1890ff",
+            // backgroundColor: "#5ebfea",
+            backgroundColor: "#2ed8b1",
+            duration: 0.4,
+            left: "-110%",
+            delay: -0.3
         })
         .to(".agency_title", {
             text: {
